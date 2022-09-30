a863db43c800e29c6a88c54bbfe0afe7ddb10e28
AliasgerKing1 <100581385+AliasgerKing1@users.noreply.github.com>
2022-09-30 04:55:00 -0700

    My Dilivery website

---
+++ Aliasger/app.js
@@ -0,0 +1,48 @@
+const show = console.log;
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+
+app.use(express.static(__dirname + "/assets"));
+
+const port = 9000;
+
+app.listen(port , ()=> {
+    show("Server Running");
+})
+
+app.get("/", (request, response)=>{
+    response.render("index.ejs");
+})
+app.get("/about", (request, response)=>{
+    response.render("about.ejs");
+})
+app.get("/contact", (request, response)=>{
+    response.render("contact.ejs");
+})
+app.get("/quote", (request, response)=>{
+    response.render("get-a-quote.ejs");
+})
+app.get("/services", (request, response)=>{
+    var name = "Aliasger";
+    var city = "Indore";
+    var  age = 18;
+
+    var user = {
+        a: name,
+        b: city,
+        c: age
+    };
+    response.render("services.ejs", user);
+})
+app.get("/service-details", (request, response)=>{
+        response.render("service-details.ejs");
+    })
+app.get("/pricing", (request, response)=>{
+    response.render("pricing.ejs");
+})
+app.get("/sample-inner-page", (request, response)=>{
+    response.render("sample-inner-page.ejs");
+})
