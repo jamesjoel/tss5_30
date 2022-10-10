2ce9025aa8650401ed247626f5ca63cef67187c0
jamesjoel <jamesjoelj@gmail.com>
2022-10-10 19:02:28 +0530

    theme in project

---
+++ project/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname+"/assets"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res)=>{
+   res.render("pages/index");
+})
+
+app.get("/about", (req, res)=>{
+  res.render("pages/about");
+})
+
+app.get("/contact", (req, res)=>{
+    res.render("pages/contact");
+})
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, ()=>{
+    console.log("server running");
+})
