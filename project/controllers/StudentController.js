76bb683c72770188cd93cecdcead31a0a931e0df
jamesjoel <jamesjoelj@gmail.com>
2022-10-14 19:01:02 +0530

    insert and find

--- project/controllers/StudentController.js
+++ project/controllers/StudentController.js
@@ -1,24 +1,23 @@
 const routes = require("express").Router();
-const MongoClient = require("mongodb").MongoClient;
+const Student = require("../models/Student");
 
 routes.get("/", (req, res)=>{
     res.render("pages/student");
 })
 
 routes.post("/save", (req, res)=>{
-    // console.log(req.body);
-
-    req.body.full_name = req.body.fullname;
-    delete req.body.fullname;
-
     req.body.fee = parseInt(req.body.fee);
     req.body.age = parseInt(req.body.age);
-    MongoClient.connect("mongodb://localhost:27017", (err, con)=>{
-        const db = con.db("tss_new");
-        db.collection("student").insertOne(req.body, (err)=>{
-            res.redirect("/");
-        });
-    });
+    Student.create(req.body, (err)=>{
+        res.redirect("/student/list");
+    })
 });
+
+routes.get("/list", (req, res)=>{
+    Student.find({}, (err, result)=>{
+        var pagedata = { result : result };
+        res.render("pages/student-list", pagedata);
+    });
+})
 
 module.exports = routes;
