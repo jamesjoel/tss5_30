6b82e4c0824d2170338ff1376e0cbf11d3f41b3c
poojacoder123 <agrawalpooja727@gmail.com>
2022-10-17 10:18:09 +0530

    pooja

--- pooja/project2/config/routes.js
+++ pooja/project2/config/routes.js
@@ -5,6 +5,6 @@ const routes = require("express").Router()
 routes.use("/", require("../controllers/HomeController"));
 routes.use("/about", require("../controllers/AboutController"));
 routes.use("/contact" , require("../controllers/ContactController"));
-routes.use("/student", require("../controllers/StudentController"));
+routes.use("/students", require("../controllers/StudentController"));
 
 module.exports = routes;
