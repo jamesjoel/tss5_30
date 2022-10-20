ec572ab5afd5eef0370b07773c83eb69b2483495
poojacoder123 <agrawalpooja727@gmail.com>
2022-10-20 12:29:21 +0530

    pooja

--- pooja/project2/config/routes.js
+++ pooja/project2/config/routes.js
@@ -6,8 +6,7 @@ const routes = require("express").Router();
 routes.use("/", require("../controllers/HomeController"));
 routes.use("/about", require("../controllers/AboutController"));
 routes.use("/contact", require("../controllers/ContactController"));
-routes.use("/Student
-", require("../controllers/StudentController"));
+routes.use("/Student", require("../controllers/StudentController"));
 
 
 
--- pooja/project3mongo/controllers/EmployeeController.js
+++ pooja/project3mongo/controllers/EmployeeController.js
@@ -5,18 +5,24 @@ const routes = require("express").Router()
     res.render("pages/employee")
  })
  routes.get("/list" , (req,res)=>{
-   Employess.find({} , (err, result)=>{
-      var pagedata ={result : result}
-      res.render("pages/employee-list", pagedata)
+   Employee.find({}, (err,result)=>{
+      // console.log(result)
+      // inserting result in variable
+      var pagedata = {
+         result:result
+      }
+      res.render("pages/employee-list",pagedata)
    })
- })
+   })
+
 // //  this is the form post method routing
 
 routes.post("/", (req,res)=>{
 console.log(req.body)
-// Employee.create(req.body, (err)=>{
-   // res.redirect("/employees/list")
+ Employee.create(req.body, (err)=>{
+   res.redirect("/employee/list")
    
+})
 })
 
  module.exports=routes;
