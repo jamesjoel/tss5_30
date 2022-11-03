e6f0381676733377e4998f7a5a57f01d0788b236
jamesjoel <jamesjoelj@gmail.com>
2022-11-03 18:35:18 +0530

    component in angular

--- pro1/src/app/app.module.ts
+++ pro1/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BoxComponent } from './box/box.component';
+import { MiniBoxComponent } from './mini-box/mini-box.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HelpusComponent } from './helpus/helpus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoxComponent,
+    MiniBoxComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    HelpusComponent
   ],
   imports: [
     BrowserModule,
